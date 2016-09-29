3b9eec2782acc88832acefa4d21550b2da92e775
JuleLaryushina <larja1994@gmail.com>
2016-09-29 10:43:15 +0300

    Скрипт для сбора статей #2
    Продолжая Shashkin-tradition "А давайте переходить к интеллектуальной части".
    По ссылке можно найти датасет на ~19к статей: абстракты, ссылки, тексты + куча метаданных.
    https://drive.google.com/drive/folders/0B0p-Nzv53V-gMXh0WHRmSEFWMWs
    
    Скоро здесь же появятся статьи только по медицине (запрос от ВВ).

---
+++ search_plos_example.R
@@ -0,0 +1,12 @@
+require(rplos)
+require(dplyr)
+
+res <- searchplos(
+  q = '*:*', limit = 20000, #q = 'medicine'
+  fl = plosfields$field,
+  fq = list('doc_type:full', '-article_type:correction','-article_type:viewpoints')
+)
+res$data %<>% filter(!is.na(abstract) & abstract != "")
+saveRDS(res$data %<>% filter(!is.na(abstract) & abstract != ""), "Data/no_spec_topic_articles")
+rm(res)
+gc()
